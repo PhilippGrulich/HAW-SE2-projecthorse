e3157fb78436ecd0a3b890a99504f26ea58c79bf
Philipp <philippgrulich@hotmail.de>
2014-05-29 12:40:32 +0200

    Test

--- android/src/com/haw/projecthorse/android/AndroidLauncher.java
+++ android/src/com/haw/projecthorse/android/AndroidLauncher.java
@@ -12,5 +12,6 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new CoreGameMain(), config);
+		System.out.println("Hallo");
 	}
 }
