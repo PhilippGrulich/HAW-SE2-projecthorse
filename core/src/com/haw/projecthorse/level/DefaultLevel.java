a26d385f51f4ab9466383d34c13e3e351438e329
Viktor <djvw1983@googlemail.com>
2014-09-22 17:51:16 +0200

    Viele Verbesserungen am Player Objekt, immernoch unfertig. Player ist jetzt ein Actor ! Default Level angepasst zum testen

--- core/src/com/haw/projecthorse/level/DefaultLevel.java
+++ core/src/com/haw/projecthorse/level/DefaultLevel.java
@@ -2,9 +2,16 @@ package com.haw.projecthorse.level;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
 import com.haw.projecthorse.assetmanager.AssetManager;
+import com.haw.projecthorse.player.Direction;
+import com.haw.projecthorse.player.Player;
+import com.haw.projecthorse.player.PlayerImpl;
 
 public class DefaultLevel extends Level {
+	
+	private Stage stage;
 
 	@Override
 	public void doRender(float delta) {
@@ -13,6 +20,8 @@ public class DefaultLevel extends Level {
 		AssetManager.showTexture("pictures/cc-0/worldmap.png", 0, 0);
 		AssetManager.showTexture("pictures/selfmade/logo.png", 0, 0);
 
+		stage.act(delta);
+		stage.draw();
 	}
 
 	@Override
@@ -26,6 +35,16 @@ public class DefaultLevel extends Level {
 		AssetManager.load();
 		AssetManager.playSound("sounds/flap.wav");
 		AssetManager.playMusic("music/life.mp3");
+		
+		stage = new Stage(getViewport());
+		Player player = new PlayerImpl();
+		player.setPosition(100, 100);
+		player.setAnimation(Direction.UPRIGHT, 0.3f);
+		ScaleToAction scale = new ScaleToAction();
+		scale.setScale(5f);
+		scale.setDuration(20f);
+		player.addAction(scale);
+		stage.addActor(player);
 
 	}
 
--- core/src/com/haw/projecthorse/player/Player.java
+++ core/src/com/haw/projecthorse/player/Player.java
@@ -1,77 +1,43 @@
 package com.haw.projecthorse.player;
 
-import com.badlogic.gdx.math.Polygon;
-import com.badlogic.gdx.math.collision.BoundingBox;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public interface Player {
-	
+/**
+ * Player ist eine spezielle Implemetierung der LibGDX Klasse Actor.
+ * Um sie sinnvoll nutzen zu können, sollte die Scene2D Stage Klasse
+ * als Parent verwendet werden.
+ * 
+ *  Siehe 
+ *  http://www.gamefromscratch.com/post/2013/12/09/LibGDX-Tutorial-9-Scene2D-Part-2-Actions.aspx
+ *  https://github.com/libgdx/libgdx/wiki/Scene2d
+ *  
+ * @author Olli, Viktor
+ *
+ */
+
+public abstract class Player extends Actor{
 	/**
-	 * Methode zum Rendern des Spielers (Pferd)
-	 * @param timeDeltaSum Aufsummierte Zeit seit Rendering Beginn 
-	 */
-	public void draw(float timeDeltaSum);
-	
-	/**
-	 * Methode zum setzen der X Koordinate
-	 * @param x
-	 */
-	public void setX(float x);
-	
-	/**
-	 * Methode zum setzen der Y Koordinate
-	 * @param y
-	 */
-	public void setY(float y);
-	
-	/**
-	 * Gibt die X Koordinate des Spielers zurück
-	 */
-	public float getX();
-	
-	/**
-	 * Gibt die Y Koordinate des Spielers zurück
-	 */
-	public float getY();
-	
-	/**
-	 * Bewegt den Spieler in eine Richtung, steuert Animation und
-	 * ggf. auch die Koordinaten (es sei denn der Spieler ist unbeweglich)
+	 * Setzt und startet eine endlose Bewegungsanimation für den Spieler
 	 * @param direction Richtung der Bewegung
 	 * @param speed Geschwindigkeit zwischen 0 und 1
 	 */
-	public void setMovement(Direction direction, float speed);
+	public abstract void setAnimation(Direction direction, float speed);
 	
 	/**
-	 * Verändert die Geschwindigkeit um ein angegegbenes Delta, die
+	 * Verändert die Geschwindigkeit der Bewegungsanimation um ein angegegbenes Delta, die
 	 * Richtung wird hierbei berücksichtigt
 	 * @param delta zwischen -1 und 1, negative Werte bremsen, positive Werte beschleunigen
 	 */
-	public void changeSpeed(float delta);
+	public abstract void changeAnimationSpeed(float delta);
 	
 	/**
-	 * @return Geschwindigkeit der aktuellen Bewegung
+	 * @return Geschwindigkeit der aktuellen Bewegungsanimation
 	 */
-	public float getSpeed();
+	public abstract float getAnimationSpeed();
 	
 	/**
 	 * 
 	 * @return True bei Bewegung, ansonsten False
 	 */
-	public boolean isMoving();
-	
-	/**
-	 * Liefert ein Polygon, das als Bounding Box verwendet werden kann
-	 * @return 
-	 */
-	public Polygon getBoundingPolygon();
-	
-	/**
-	 * Liefert eine rechteckige Bounding Box, kann sehr ungenau sein!
-	 * Für genauere Berechnungen sollte getBoundingPolygon() genutzt werden
-	 * @return
-	 */
-	public BoundingBox getBoundingBox();
-	
-	
-
+	public abstract boolean isMoving();
 }
