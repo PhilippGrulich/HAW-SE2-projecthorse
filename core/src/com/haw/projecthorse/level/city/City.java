3b927ca8aeac694ae8c7969d4bc86d81b4dd5c32
Philipp Grulich <philippgrulich@hotmail.de>
2014-09-26 11:10:17 +0200

    City: Refactoring

--- core/src/com/haw/projecthorse/level/city/City.java
+++ core/src/com/haw/projecthorse/level/city/City.java
@@ -1,24 +1,20 @@
 package com.haw.projecthorse.level.city;
 
-import com.badlogic.gdx.Gdx; 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton.ImageTextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.haw.projecthorse.assetmanager.AssetManager;
 import com.haw.projecthorse.gamemanager.GameManagerFactory;
 import com.haw.projecthorse.gamemanager.navigationmanager.exception.LevelNotFoundException;
@@ -26,23 +22,24 @@ import com.haw.projecthorse.gamemanager.navigationmanager.json.CityObject;
 import com.haw.projecthorse.gamemanager.navigationmanager.json.GameObject;
 import com.haw.projecthorse.level.Level;
 
-public class city extends Level {
+public class City extends Level {
 
-    
-	private Texture texture;
 	private Stage stage;
 	private TextureAtlas atlant;
 	private SpriteBatch batcher = new SpriteBatch();
 	private CityObject cityObject;
-	
-	private int lastButtonY= GameManagerFactory.getInstance().getSettings().getScreenHeight();
+
+	private int lastButtonY = GameManagerFactory.getInstance().getSettings().getScreenHeight();
 	private BitmapFont font;
 	private AtlasRegion region;
+	private ImageTextButtonStyle imageButtonStyle;
+
 	@Override
-	protected void doShow() { 
+	protected void doShow() {
 		// TODO Auto-generated method stub
 
 		atlant = AssetManager.load("hamburg", false, false, true);
+		
 		stage = new Stage(this.getViewport(), this.getSpriteBatch());
 		try {
 			cityObject = GameManagerFactory.getInstance().getCityObject(getLevelID());
@@ -52,82 +49,69 @@ public class city extends Level {
 			e1.printStackTrace();
 		}
 
-		 region = atlant.findRegion("Sankt-Michaelis-Kirche_Hamburg");
+		region = atlant.findRegion("Sankt-Michaelis-Kirche_Hamburg");
 		font = new BitmapFont(Gdx.files.internal("pictures/selfmade/font.txt"));
-		font.setScale(.45f,.45f);
+		font.setScale(.45f, .45f);
 		font.setColor(Color.MAGENTA);
 
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	
 
-
-
-		
-		Gdx.input.setInputProcessor(stage); 
+		Gdx.input.setInputProcessor(stage);
 
 	}
-	
-	private void addGameButtons() throws LevelNotFoundException{
+
+	private void addGameButtons() throws LevelNotFoundException {
+		Drawable drawable = new TextureRegionDrawable(atlant.findRegion("raw_game_teaser_icon"));
+
+		imageButtonStyle = new ImageTextButton.ImageTextButtonStyle();
+		imageButtonStyle.down = drawable;
+		imageButtonStyle.up = drawable;
+		imageButtonStyle.font = new BitmapFont();
+		imageButtonStyle.fontColor = Color.BLACK;
+
 		GameObject[] games = cityObject.getGames();
-		for(GameObject game : games){			
+		for (GameObject game : games) {
 			addGameButton(game);
 		}
+		imageButtonStyle = null;
 	}
-	
-	private void addGameButton(final GameObject gameObject){
-		Drawable drawable = new TextureRegionDrawable(atlant.findRegion("raw_game_teaser_icon"));
-		
-		ImageTextButton.ImageTextButtonStyle style = new ImageTextButton.ImageTextButtonStyle();
-		style.down = drawable;
-		style.up = drawable;
-		style.font = new BitmapFont();
-		style.fontColor = Color.BLACK;
-		ImageTextButton imgTextButton = new ImageTextButton(gameObject.getGameTitle(),style);
-		
-		
-		
+
+	private void addGameButton(final GameObject gameObject) {
+
+		ImageTextButton imgTextButton = new ImageTextButton(gameObject.getGameTitle(), imageButtonStyle);
+
 		imgTextButton.addListener(new ChangeListener() {
 			public void changed(ChangeEvent event, Actor actor) {
-				System.out.println("Spiel "+ gameObject.getGameTitle() + " soll gestartet werden");
+				System.out.println("Spiel " + gameObject.getGameTitle() + " soll gestartet werden");
 				GameManagerFactory.getInstance().navigateToLevel(gameObject.getLevelID());
 
 			}
 		});
-		
-		
+
 		imgTextButton.setPosition(200, lastButtonY);
-		lastButtonY=lastButtonY-200;
+		lastButtonY = lastButtonY - 200;
 		stage.addActor(imgTextButton);
 	};
+
 	@Override
 	protected void doRender(float delta) {
-	
-		// TODO Auto-generated method stub
-	
-		
-		batcher.begin();
-		batcher.draw(region,
-				0, 0, GameManagerFactory.getInstance().getSettings().getScreenWidth(), GameManagerFactory.getInstance().getSettings().getScreenHeight());	
-		font.draw(batcher, cityObject.getCityName(), Gdx.graphics.getWidth()/1.8f, Gdx.graphics.getHeight()/1.03f);
-		batcher.end();	
-		
-		stage.draw();
-//		SpriteBatch batch1 = new SpriteBatch();
-//		batch1.begin();
-//		batch1.setColor(Color.GREEN);
-//		batch1.draw(texture, 100, 100,350, 128);				
-//		batch1.end();
-//	
-		
-		
-	}
-	
 
+
+		batcher.begin();
+		batcher.draw(region, 0, 0, GameManagerFactory.getInstance().getSettings().getScreenWidth(), GameManagerFactory.getInstance().getSettings().getScreenHeight());
+		font.draw(batcher, cityObject.getCityName(), Gdx.graphics.getWidth() / 1.8f, Gdx.graphics.getHeight() / 1.03f);
+		batcher.end();
+		stage.draw();
+		
+
+	}
 
 	@Override
 	protected void doDispose() {
-		// TODO Auto-generated method stub
+		atlant.dispose();
+		font.dispose();
+		
 
 	}
 
@@ -136,8 +120,6 @@ public class city extends Level {
 		// TODO Auto-generated method stub
 
 	}
-
-	
 
 	@Override
 	protected void doHide() {
