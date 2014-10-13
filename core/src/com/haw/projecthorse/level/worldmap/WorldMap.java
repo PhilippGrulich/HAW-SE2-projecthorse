70349e664c1aa75c1b5b0c34265f61c096ea837d
MariaAI <maria-kor@web.de>
2014-10-13 15:02:28 +0200

    es navigiert zunächst nur nach Hamburg

--- core/src/com/haw/projecthorse/level/worldmap/WorldMap.java
+++ core/src/com/haw/projecthorse/level/worldmap/WorldMap.java
@@ -1,79 +1,104 @@
 package com.haw.projecthorse.level.worldmap;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.haw.projecthorse.assetmanager.AssetManager;
+import com.haw.projecthorse.level.Level;
 import com.haw.projecthorse.gamemanager.GameManagerFactory;
 import com.haw.projecthorse.intputmanager.InputManager;
-import com.haw.projecthorse.level.Level;
 
 public class WorldMap extends Level {
 
 	private Stage stage;
-	private Skin skin;
-	private Pixmap pixmap;
-	private TextButtonStyle buttonStyle;
-	private TextButton stadtButton;
-	//private ImageButton hhFlagge;
-	private TextureAtlas germanyatlas;
-	private AtlasRegion germanytexture;
-	private Image background;
+	final Image worldmapimage;
+	boolean bool = false;
+	final TextureAtlas worldmapatlas;
+
 	public WorldMap() {
-		super(); 
+		super();
+
+		worldmapatlas = AssetManager.load("worldmap", false, false, true);
+
 		stage = new Stage(getViewport());
 		InputManager.addInputProcessor(stage);
-		germanyatlas = AssetManager.load("worldmap", false, false, true);
-		germanytexture = germanyatlas.findRegion("germanymap_scaled");
-		//texture.getTexture();
-       // hhFlagge = new ImageButton();
-		skin = new Skin();
-		//skin.addRegions(texture);
-		pixmap = new Pixmap(1, 1, Format.RGBA8888);
-		pixmap.setColor(Color.WHITE);
-		pixmap.fill();
-		skin.add("white", new Texture(pixmap));
-		pixmap.dispose();
-	
-	//	hhFlage = new ImageButton();
-		buttonStyle = new TextButtonStyle();
-		buttonStyle.up = skin.newDrawable("white", Color.PINK);
-		skin.add("default", new BitmapFont());
-		buttonStyle.font = skin.getFont("default");
-		stadtButton = new TextButton("HH", buttonStyle);// erzeugen
-		stadtButton.setX(0.432f * width);
-		stadtButton.setY(0.7f * height );
-		stadtButton.addListener(new ChangeListener() {
-			public void changed(ChangeEvent event, Actor actor) {
-				GameManagerFactory.getInstance().navigateToLevel("Hamburg");
-			}
-		});
-		background = new Image(germanytexture);
-		background.toBack();
-		background.setY((height - background.getHeight())/2);
-		stage.addActor(background);
-		stage.addActor(stadtButton);
+
+		worldmapimage = addBackground();
+
+		final ImageButton imagebutton1 = createImageButton("berlinwappen",
+				width / 2f, 0f);
+		ImageButton imagebutton2 = createImageButton("hamburgwappen",
+				width / 2f, height / 2f);
+
+		addListener(imagebutton1);
+		addListener(imagebutton2);
+
+		stage.addActor(imagebutton1);
+		stage.addActor(imagebutton2);
 
 	}
 
+	private Image addBackground() {
+
+		AtlasRegion wolrldmapatlasregion = worldmapatlas
+				.findRegion("erde-und-sterne");
+		Image worldmapimage = new Image(wolrldmapatlasregion);
+		worldmapimage.toBack();
+		worldmapimage.setY((height - worldmapimage.getHeight()) / 2);
+		stage.addActor(worldmapimage);
+		return worldmapimage;
+
+	}
+
+	private ImageButton createImageButton(String imagename, float x, float y) {
+		Drawable drawable = new TextureRegionDrawable(new TextureRegion(
+				new Texture(Gdx.files.internal("pictures/wappen/" + imagename
+						+ ".png"))));
+		ImageButton buttonFlagge = new ImageButton(drawable);
+		buttonFlagge.setHeight(200);
+		buttonFlagge.setWidth(300);
+		buttonFlagge.setX(x);
+		buttonFlagge.setY(y);
+		return buttonFlagge;
+	}
+
+	private void addListener(final ImageButton imagebutton) {
+		imagebutton.addListener(new ChangeListener() {
+
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+
+				imagebutton.setVisible(false);
+				worldmapimage.setOriginX(0.643f * width);
+				worldmapimage.setOriginY(0.65f * height);
+
+				System.out.println("hier navigiere");
+				GameManagerFactory.getInstance().navigateToLevel("Hamburg");//.navigateToMainMenu();
+
+			}
+
+		});
+
+	}
+
+	
+
 	@Override
 	public void doRender(float delta) {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act(delta);
+		stage.act(delta);// akktualisiere
 		stage.draw();
 	}
 
@@ -85,8 +110,8 @@ public class WorldMap extends Level {
 	@Override
 	public void doShow() {
 		// TODO Auto-generated method stub
-	//	AssetManager.load();
-		//AssetManager.playSound("sounds/flap.wav");
+		// AssetManager.load();
+		// AssetManager.playSound("sounds/flap.wav");
 		// AssetManager.playMusic("music/life.mp3");
 
 	}
@@ -112,7 +137,6 @@ public class WorldMap extends Level {
 	@Override
 	public void doDispose() {
 		stage.dispose();
-		skin.dispose();
 	}
 
 }
