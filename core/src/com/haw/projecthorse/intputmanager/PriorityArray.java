243608ad8285a8b44632a7fca28106f17d1b2843
Philipp Grulich <philippgrulich@hotmail.de>
2014-10-22 08:58:23 +0200

    Update

--- core/src/com/haw/projecthorse/intputmanager/PriorityArray.java
+++ core/src/com/haw/projecthorse/intputmanager/PriorityArray.java
@@ -1,57 +1,53 @@
 package com.haw.projecthorse.intputmanager;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class PriorityArray<E> implements Iterable<E> {
-	
+
 	List<Content<E>> list = new ArrayList<Content<E>>();
 	int prio = 0;
-	private class Content<E> implements Comparable<Content>{
-		private E content; 
+
+	private class Content<T> implements Comparable<Content<T>> {
+		private T content;
 		public Integer priority;
-		public Content(int prio, E element) {
+
+		public Content(int prio, T element) {
 			priority = prio;
 			content = element;
 		}
 
-		public E getElement(){
+		public T getElement() {
 			return content;
 		}
+
 		@Override
-		public int compareTo(Content o) {
+		public int compareTo(Content<T> o) {
 			return o.priority.compareTo(priority);
 		}
-		
-	}
-	
-	
 
-	
-	public boolean add(E e,int prio) {
+	}
+
+	public boolean add(E e, int prio) {
 		return list.add(new Content<E>(prio, e));
 	}
-	
-	
-	
+
 	public void clear() {
 		list.clear();
 
-	}	
-
+	}
 
 	@Override
-	public Iterator<E> iterator() {		
+	public Iterator<E> iterator() {
 		Collections.sort(list);
 		Iterator<E> i = new Iterator<E>() {
 			int index = 0;
 
 			@Override
 			public boolean hasNext() {
-				return list.size()> index;
+				return list.size() > index;
 			}
 
 			@Override
@@ -60,15 +56,18 @@ public class PriorityArray<E> implements Iterable<E> {
 				index++;
 				return e;
 			}
+
+			@Override
+			public void remove() {
+				list.remove(index);
+
+			}
 		};
 		return i;
 	}
 
-	
-	
 	public int size() {
 		return list.size();
 	}
 
-	
 }
