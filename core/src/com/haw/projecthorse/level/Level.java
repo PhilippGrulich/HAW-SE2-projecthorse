d88c99188aaca2b2cd20e4cc45362f1f0875cfa3
Viktor <djvw1983@googlemail.com>
2014-10-25 17:21:40 +0200

    fixed #45 - Schwarz als Standard Hintergrundfarbe

--- core/src/com/haw/projecthorse/level/Level.java
+++ core/src/com/haw/projecthorse/level/Level.java
@@ -1,6 +1,8 @@
 package com.haw.projecthorse.level;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -76,9 +78,10 @@ public abstract class Level implements Screen {
 		if(paused){ 
 			delta = 0;
 			};
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);	    
 		doRender(delta);		
 		overlay.draw();
-	
 	}
 
 	protected abstract void doDispose();
