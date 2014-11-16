cbd9c02abb38d9fc5027b55123ae39c5115381f8
Oliver <oliver.niebsch@haw-hamburg.de>
2014-11-16 20:42:16 +0100

    Logikfehler fürs Loot gefixed - Issue #88

--- core/src/com/haw/projecthorse/lootmanager/Chest.java
+++ core/src/com/haw/projecthorse/lootmanager/Chest.java
@@ -12,12 +12,12 @@ import com.haw.projecthorse.savegame.SaveGameManager;
 
 public class Chest {
 	private ArrayList<Loot> content = null;
-	private Overlay overlay;	
-	
+	private Overlay overlay;
+
 	public Chest(Overlay overlay) {
 		this.overlay = overlay;
 	}
-	
+
 	public void addLoot(Loot loot) {
 		if (content == null) {
 			content = new ArrayList<Loot>();
@@ -25,28 +25,33 @@ public class Chest {
 		loot.achievedDate = new Date();
 		content.add(loot);
 	}
-	
+
 	/**
 	 * Zeig das bisher gesammelte Loot in einem Popup an.
 	 */
 	public void showAllLoot() {
-		overlay.showPopup(new LootPopup(content));
-		InputManager.addInputProcessor(new StageGestureDetector(overlay, false, ControlMode.HORIZONTAL), Integer.MAX_VALUE);
+		if (content != null) {
+			overlay.showPopup(new LootPopup(content));
+			InputManager.addInputProcessor(new StageGestureDetector(overlay,
+					false, ControlMode.HORIZONTAL), Integer.MAX_VALUE);
+		}
 	}
-	
+
 	/**
 	 * Speichert das bisher gesammelte Loot und leert die Chest.
 	 */
 	public void saveAllLoot() {
-		SaveGameManager.getLoadedGame().addCollectedLootList(content);
-		SaveGameManager.saveLoadedGame();
-		
-		content.clear();
+		if (content != null) {
+			SaveGameManager.getLoadedGame().addCollectedLootList(content);
+			SaveGameManager.saveLoadedGame();
+
+			content.clear();
+		}
 	}
-	
+
 	public void addLootAndShowAchievment(Loot loot) {
 		addLoot(loot);
-		
+
 		String message = loot.getDescription();
 		if (message != null) {
 			// TODO: Erfolg enzeigen
