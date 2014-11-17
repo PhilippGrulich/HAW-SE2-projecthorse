0125617c7c08ca8985e6c0ba2a782acea7a6dfcc
Philipp Grulich <philippgrulich@hotmail.de>
2014-11-17 20:15:11 +0100

    Fix für Samsung Geräte ohne Navbar

--- android/src/com/haw/projecthorse/android/AndroidLauncher.java
+++ android/src/com/haw/projecthorse/android/AndroidLauncher.java
@@ -3,6 +3,7 @@ package com.haw.projecthorse.android;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -14,13 +15,21 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Hiermit wird die Android Navigation Bar ausgeblendet, zurückholen
+		// Hiermit wird die Android Navigation Bar ausgeblendet, zurï¿½ckholen
 		// geht ab Android 4.4 mit einem Swipe vom oberen Bildschirmrand nach unten
 
 		if (Build.VERSION.SDK_INT >= 19) {
-			View decorView = getWindow().getDecorView();
-			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+			Window window = getWindow();
+			if(window!=null){
+			try {
+				View decorView = window.getDecorView();
+				decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+			} catch (Exception e) {
+				// No navbar on device
+			}
+			
+			}
 		}
 
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
