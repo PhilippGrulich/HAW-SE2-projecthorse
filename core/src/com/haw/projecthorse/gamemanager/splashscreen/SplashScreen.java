3cf1cfdf722e890256af13ba6bbb8e95db4001a6
Philipp Grulich <philippgrulich@hotmail.de>
2014-11-26 17:09:07 +0100

    Fix #99

--- core/src/com/haw/projecthorse/gamemanager/splashscreen/SplashScreen.java
+++ core/src/com/haw/projecthorse/gamemanager/splashscreen/SplashScreen.java
@@ -7,6 +7,7 @@ import java.util.Random;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -120,6 +121,8 @@ public class SplashScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		updateText(delta);
 		stage.draw();
 		
