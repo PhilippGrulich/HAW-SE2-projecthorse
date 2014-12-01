6c19c32c81c56396d2db3c4f2a420382640edc94
Philipp Grulich <philippgrulich@hotmail.de>
2014-12-01 22:11:59 +0100

    Erster Cursor hinzugefügt

--- core/src/com/haw/projecthorse/level/util/uielements/TextInputField.java
+++ core/src/com/haw/projecthorse/level/util/uielements/TextInputField.java
@@ -2,8 +2,10 @@ package com.haw.projecthorse.level.util.uielements;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.haw.projecthorse.assetmanager.AssetManager;
 import com.haw.projecthorse.assetmanager.FontSize;
+import com.haw.projecthorse.level.game.parcours.Text;
 
 /**
  * 
@@ -27,7 +29,7 @@ public class TextInputField extends TextField {
 		textFieldStyle.font = AssetManager.getTextFont(FontSize.VIERZIG);
 		textFieldStyle.fontColor = Color.LIGHT_GRAY;
 		textFieldStyle.focusedFontColor = Color.GRAY;
-		
+		textFieldStyle.cursor = new TextureRegionDrawable(AssetManager.getTextureRegion("ui", "cursor"));
 		return textFieldStyle;
 	}
 
