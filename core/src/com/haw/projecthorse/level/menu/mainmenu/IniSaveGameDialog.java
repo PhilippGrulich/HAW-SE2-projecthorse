701ec925fb34879069eaf5ca1319720fc98b735b
Philipp Grulich <PhilippGrulich@hotmail.de>
2014-12-02 10:52:53 +0100

    TextInput Fix

--- core/src/com/haw/projecthorse/level/menu/mainmenu/IniSaveGameDialog.java
+++ core/src/com/haw/projecthorse/level/menu/mainmenu/IniSaveGameDialog.java
@@ -1,8 +1,11 @@
 package com.haw.projecthorse.level.menu.mainmenu;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.haw.projecthorse.gamemanager.GameManagerFactory;
+import com.haw.projecthorse.level.util.overlay.Overlay;
 import com.haw.projecthorse.level.util.overlay.popup.Dialog;
 import com.haw.projecthorse.level.util.uielements.TextInputField;
 import com.haw.projecthorse.savegame.SaveGameManager;
@@ -13,33 +16,34 @@ public class IniSaveGameDialog extends Dialog {
 	private TextInputField textInput;
 
 	/**
-	 * Dialog für um ein Neuen Spieler anzulegen. 
-	 * @param  saveGameID Id des SaveGames
+	 * Dialog für um ein Neuen Spieler anzulegen.
+	 * 
+	 * @param saveGameID
+	 *            Id des SaveGames
 	 */
 	public IniSaveGameDialog(final int saveGameID) {
 		super("Hallo Reisende. \n Wie lautet dein Name?");
 
-		textInput = new TextInputField("Name");
-		textInput.setBlinkTime(1);
-		textInput.setCursorPosition(0);
+		textInput = new TextInputField("");
 		textInput.setFillParent(true);
 		textInput.setRightAligned(false);
 		textInput.setWidth(popupWidth - 100);
 		textInput.setMaxLength(20);
 
-		//getOverlay().setKeyboardFocus(textInput);
-		//show the keyboard
+		// show the keyboard
 		textInput.getOnscreenKeyboard().show(true);
-		
+
 		addActor(textInput);
 		addButton("Los", new ChangeListener() {
 
 			@Override
 			public void changed(final ChangeEvent event, final Actor actor) {
-				SaveGame game = SaveGameManager.loadSavedGame(saveGameID);
-				game.setPlayerName(textInput.getText());
-				SaveGameManager.saveLoadedGame();
-				GameManagerFactory.getInstance().navigateToWorldMap();
+				if (!textInput.getText().equals("")) {
+					SaveGame game = SaveGameManager.loadSavedGame(saveGameID);
+					game.setPlayerName(textInput.getText());
+					SaveGameManager.saveLoadedGame();
+					GameManagerFactory.getInstance().navigateToWorldMap();
+				}
 
 			}
 		});
@@ -52,6 +56,15 @@ public class IniSaveGameDialog extends Dialog {
 
 			}
 		});
+	}
+
+	@Override
+	protected void setStage(Stage stage) {
+		// TODO Auto-generated method stub
+		super.setStage(stage);
+		Overlay o = getOverlay();
+		if (o != null)
+			o.setKeyboardFocus(textInput);
 	}
 
 }
