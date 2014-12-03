393ffc013bfc69f20678c965598a82a992e42707
Philipp Grulich <philippgrulich@hotmail.de>
2014-12-03 15:09:04 +0100

    Neue NavButtons

--- core/src/com/haw/projecthorse/level/util/overlay/navbar/MenuNavBar.java
+++ core/src/com/haw/projecthorse/level/util/overlay/navbar/MenuNavBar.java
@@ -1,16 +1,22 @@
 package com.haw.projecthorse.level.util.overlay.navbar;
 
 import com.haw.projecthorse.level.util.overlay.navbar.button.NavbarBackButton;
+import com.haw.projecthorse.level.util.overlay.navbar.button.NavbarLootGalleryButton;
 import com.haw.projecthorse.level.util.overlay.navbar.button.NavbarSettingsButton;
+import com.haw.projecthorse.level.util.overlay.navbar.button.NavbarStableButton;
 
 public class MenuNavBar extends NavBar {
 
 	public MenuNavBar() {
 
-
 		NavbarSettingsButton settingsButton = new NavbarSettingsButton();
 		addButton(settingsButton);
-		
+
+		NavbarLootGalleryButton lootButton = new NavbarLootGalleryButton();
+		addButton(lootButton);
+		NavbarStableButton homeButton = new NavbarStableButton();
+		addButton(homeButton);
+
 		NavbarBackButton pauseButton = new NavbarBackButton();
 		addButton(pauseButton);
 
---
+++ core/src/com/haw/projecthorse/level/util/overlay/navbar/button/NavbarLootGalleryButton.java
@@ -0,0 +1,29 @@
+package com.haw.projecthorse.level.util.overlay.navbar.button;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.haw.projecthorse.level.util.uielements.ButtonSmall;
+
+public class NavbarLootGalleryButton extends NavbarButton {
+
+	public NavbarLootGalleryButton() {
+		ImageButton imageButton = new ButtonSmall(ButtonSmall.ButtonType.LOOT);
+
+		this.addActor(imageButton);
+		this.addListener(new InputListener() {
+
+			@Override
+			public boolean touchDown(
+					com.badlogic.gdx.scenes.scene2d.InputEvent event, float x,
+					float y, int pointer, int button) {
+						Gdx.app.log("Settings Button", "Settings BUTTON CLick");
+				
+
+				return true;
+			};
+
+		});
+		this.setWidth(imageButton.getHeight());
+	}
+}
