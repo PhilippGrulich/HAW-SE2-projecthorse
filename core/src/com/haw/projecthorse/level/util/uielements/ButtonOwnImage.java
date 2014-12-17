43f96f4d23f61eed7ec366bc012d5c85379749eb
Viktor <djvw1983@googlemail.com>
2014-12-17 19:04:23 +0100

    Fixed #134: CityPoints in Worldmap sind nun auch Navigationsbuttons

--- core/src/com/haw/projecthorse/level/util/uielements/ButtonOwnImage.java
+++ core/src/com/haw/projecthorse/level/util/uielements/ButtonOwnImage.java
@@ -8,36 +8,42 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.haw.projecthorse.audiomanager.AudioManagerImpl;
 
-public class ButtonOwnImage extends ImageButton{
-	
+public class ButtonOwnImage extends ImageButton {
+
 	/**
-	 * @author Viktor
-	 *	Diese Klasse ist kapselt den LibGDX ImageButton und erweitert ihn sinnvoll.
+	 * @author Viktor Diese Klasse ist kapselt den LibGDX ImageButton und
+	 *         erweitert ihn sinnvoll.
 	 */
 
-	public ButtonOwnImage(Drawable imageUp){
+	public ButtonOwnImage(Drawable imageUp) {
 		super(imageUp);
 		addFeedback();
+		setTransform(true); // Workaround für falsche Implementierung oder
+							// Dokumentation in LibGDX
 	}
-	
-	public ButtonOwnImage(Skin skin){
+
+	public ButtonOwnImage(Skin skin) {
 		super(skin);
 		addFeedback();
+		setTransform(true); // Workaround für falsche Implementierung oder
+							// Dokumentation in LibGDX
 	}
-	
-	public ButtonOwnImage(ImageButtonStyle style){
+
+	public ButtonOwnImage(ImageButtonStyle style) {
 		super(style);
 		addFeedback();
+		setTransform(true); // Workaround für falsche Implementierung oder
+							// Dokumentation in LibGDX
 	}
-	
-	private void addFeedback(){
+
+	private void addFeedback() {
 		this.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
-			AudioManagerImpl.getInstance().getSound("ui", "click.ogg").play();
-			Gdx.input.vibrate(50);	
+				AudioManagerImpl.getInstance().getSound("ui", "click.ogg")
+						.play();
+				Gdx.input.vibrate(50);
 			}
 		});
 	}
-
 }
