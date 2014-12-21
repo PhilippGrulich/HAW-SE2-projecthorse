410b6e594a77daee3769894a4e1d5e17fcb53c2d
ngochuyen92 <ngochuyen92@gmail.com>
2014-12-21 21:57:49 +0100

    MemorySpiel: Replay PopUp wurde implementiert. Score Font wurde korrigiert.

--- core/src/com/haw/projecthorse/level/game/memoryspiel/KartenManager.java
+++ core/src/com/haw/projecthorse/level/game/memoryspiel/KartenManager.java
@@ -2,8 +2,6 @@ package com.haw.projecthorse.level.game.memoryspiel;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.haw.projecthorse.assetmanager.AssetManager;
@@ -13,7 +11,7 @@ public class KartenManager extends Thread {
 
 	private ArrayList<Karte> karten = new ArrayList<Karte>();
 	private float delta = 0;
-	private boolean flag = false;
+	private boolean timeDelay = false;
 	private Karte karteA;
 	private Karte karteB;
 	public boolean canOpen = true;
@@ -30,18 +28,19 @@ public class KartenManager extends Thread {
 	}
 
 	public void setUpKarten() {
-		Karte karte1 = new Karte(40, 75);
-		Karte karte2 = new Karte(265, 75);
-		Karte karte3 = new Karte(490, 75);
-		Karte karte4 = new Karte(40, 300);
-		Karte karte5 = new Karte(265, 300);
-		Karte karte6 = new Karte(490, 300);
-		Karte karte7 = new Karte(40, 525);
-		Karte karte8 = new Karte(265, 525);
-		Karte karte9 = new Karte(490, 525);
-		Karte karte10 = new Karte(40, 750);
-		Karte karte11 = new Karte(265, 750);
-		Karte karte12 = new Karte(490, 750);
+		int y = 75;
+		Karte karte1 = new Karte(40, 75+y);
+		Karte karte2 = new Karte(265, 75+y);
+		Karte karte3 = new Karte(490, 75+y);
+		Karte karte4 = new Karte(40, 300+y);
+		Karte karte5 = new Karte(265, 300+y);
+		Karte karte6 = new Karte(490, 300+y);
+		Karte karte7 = new Karte(40, 525+y);
+		Karte karte8 = new Karte(265, 525+y);
+		Karte karte9 = new Karte(490, 525+y);
+		Karte karte10 = new Karte(40, 750+y);
+		Karte karte11 = new Karte(265, 750+y);
+		Karte karte12 = new Karte(490, 750+y);
 		karten.add(karte1);
 		karten.add(karte2);
 		karten.add(karte3);
@@ -89,7 +88,7 @@ public class KartenManager extends Thread {
 	}
 
 	public void checkChanged(float delta) {
-		if (flag == false) {
+		if (timeDelay == false) {
 			for (Karte karte1 : karten) {
 				if (karte1.getState() == CardState.TEMPORARILY_OPENED) {
 					for (Karte karte2 : karten) {
@@ -104,12 +103,12 @@ public class KartenManager extends Thread {
 							} else {
 								karteA = karte1;
 								karteB = karte2;
-								if (flag == false) {
+								if (timeDelay == false) {
 									score -= 5;
 									if (score < 0)
 										score = 0;
 								}
-								flag = true;
+								timeDelay = true;
 							}
 						}
 					}
@@ -121,7 +120,7 @@ public class KartenManager extends Thread {
 				karteA.setState(CardState.TEMPORARILY_CLOSED);
 				karteB.setState(CardState.TEMPORARILY_CLOSED);
 				this.delta = 0;
-				flag = false;
+				timeDelay = false;
 				canOpen = true;
 			}
 		}
