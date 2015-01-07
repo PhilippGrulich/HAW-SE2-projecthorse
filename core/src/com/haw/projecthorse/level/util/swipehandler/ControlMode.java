0701004132ac05819abab6f92e0ef197da06ef85
Oliver <oliver.niebsch@haw-hamburg.de>
2015-01-07 21:22:52 +0100

    Swipehanler erfüllt jetzt die Coderichtlinien (#157)

--- core/src/com/haw/projecthorse/level/util/swipehandler/ControlMode.java
+++ core/src/com/haw/projecthorse/level/util/swipehandler/ControlMode.java
@@ -1,17 +1,16 @@
 package com.haw.projecthorse.level.util.swipehandler;
 
-import com.haw.projecthorse.player.actions.Direction;
-
 /**
- * Enum f�r die unterschiedlichen Kontroll-Modi der Swipe Gesten.
- * Siehe auch {@link Direction}
+ * Enum für die unterschiedlichen Kontroll-Modi der Swipe Gesten.
+ * Siehe auch {@link com.haw.projecthorse.player.actions.Direction}
  * 
  * HORIZONTAL: Nur Rechts und Links wird unterschieden.
  * VERTICAL: Nur Hoch und Runter wird unterschieden.
  * TWO_AXIS: Rechts, Links, Hoch und Runter werden unterschieden.
  * FOUR_AXIS: Rechts, Links, Hoch, Runter, RechtsHoch, RechtsRunter, LinksHoch und LinksRunter werden unterschieden.
+ * 
  * @author Oliver
- *
+ * @version 1.0
  */
 public enum ControlMode {
 	HORIZONTAL, VERTICAL, TWO_AXIS, FOUR_AXIS
